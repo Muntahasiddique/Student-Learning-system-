856e05aab7f26bd2aadb16c47e2b1505f0e36c6d
muntaha <muntahasiddique0@gmail.com>
2026-08-01 06:29:32 +0500

    signup: add password strength validation

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,12 @@
 const userModel = require("../models/user.model");
 const Signup = async (req , res)=>{
 const { name , email ,password,role } = req.body;
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+if(!passwordRegex.test(password)){
+    return res.status(400).json({message : "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character."})
+}
+
 const existingUser = await userModel.findOne({email});
 if(existingUser){
    return res.status(400).json({message:"already exists"});
