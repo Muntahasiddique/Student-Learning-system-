065babfb5e78baf5ef30d548b5a78f32cf0c3cab
muntaha <muntahasiddique0@gmail.com>
2026-08-30 04:15:34 +0500

    remove accidental nested duplicate folder from cherry-pick

--- Student learning System/server/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const AuthController = require('../controllers/auth.controller');
-
-router.post('/signup' , AuthController.Signup);
-router.post('/login' ,AuthController.Login )
-
-module.exports =router;
